11a94409f7dcc07a3d12c4b064821a9038ec5eee
Matt Gonzalez <matt@echoaudio.com>
2024-11-21 13:59:52 -0800

    Removed examples & tests folders

--- examples/deps.cmake
+++
@@ -1,3 +0,0 @@
-set(APP_DEPENDENT_MODULES "lib_xua"
-                          "lib_board_support(1.1.1)"
-                          )
--- tests/xua_sim_tests/test_deps.cmake
+++
@@ -1,3 +0,0 @@
-list(APPEND APP_DEPENDENT_MODULES "lib_logging(3.3.1)"
-                                  "lib_random(1.2.0)"
-                                  "lib_unity(2.6.1)")
